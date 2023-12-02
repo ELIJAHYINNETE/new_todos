89fee849605543b915267e300e5fd5b9688923d4
ELIJAHYINNETE <elijah.emdal@gmail.com>
2023-12-02 16:23:30 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+
+
+//let li =document.createElement("li");
+//li.innerHTML = inputbox.value;
+//listContainer.appendChild(li);
+//let span = document.createElement("span");
+//span.innerHTML = "\u00d7";
+//li. appendChild(span);

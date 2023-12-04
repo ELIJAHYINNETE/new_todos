7bd083f142ba42e7816ce649c923030462bb9f7a
ELIJAHYINNETE <elijah.emdal@gmail.com>
2023-12-04 14:45:22 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -1,36 +1,64 @@
-const listContainer = document.getElementById("list-container");
-const inputBox = document.getElementById("input-box");
-inputBox.addEventListener('keydown',(event) =>{
-    if(event.key ==='Enter'){
-        console.log('Enter key pressed!');
-    }
-    else{
-        let li =document.createElement("li");
-        li.innerHTML = inputbox.value;
-        listContainer.appendChild(li);
-        let span = document.createElement("span");
-        span.innerHTML = "\u00d7";
-        li. appendChild(span);
-    }
-    inputBox.value = "";
-    saveData();
-});
+function btn() {
+  var taskInput = document.getElementById('input-box');
+ if (taskInput.value.trim() !== '')
+{
+  var taskItem = document.createElement('li');
+  taskItem .className = 'todo-item';
+  var taskText = document.createElement('span');
+  taskText.textContent = input-box.value;
+  taskItem.appendChild(taskText);
 
- listContainer.addEventListener("click", function (e){
-    if(e.target.tagname === "LI"){
-        e.target.classList.toogle("checked");
-        saveData()
-    }
-    else if(e.target.tagname === "SPAN"){
-        e.target.parentElement.remove();
-        saveData()
-    }
-  },false);
 
-  function saveData(){
-    localStorage.setItem("data", listContainer.innerHTML);
+  {
+    taskList.removeChild(taskItem);
   }
-  function showTask(){
-    listContainer.innerHTML = localStorage.getItem("data");
-  }
-  showTask();
+  taskList.appendChild(taskItem);
+  taskInput.value='';
+
+}
+}
+
+
+
+
+
+
+
+
+
+
+
+//  function addTask(){
+// const listContainer = document.getElementById("list-container");
+//  var inPut = document.getElementById("newInput");
+//  if(inPut.value.trim() !== ''){
+//    var taskItem = document.childElement('li');
+//    taskItem.className = 'todo-item';
+//    var taskText = document.createElement('span');
+//   taskText.textContent = inPut.value;
+//    taskItem.appendChild(taskText);
+//  }
+//  }
+
+
+// inputBox.value = "";
+//     saveData();
+//  listContainer.addEventListener("click", function (e){
+//     if(e.target.tagname === "LI"){
+//         e.target.classList.toogle("checked");
+//         saveData()
+//     }
+//     else if(e.target.tagname === "SPAN"){
+//         e.target.parentElement.remove();
+//         saveData()
+//     }
+//   },false);
+
+//   function saveData(){
+//     localStorage.setItem("data", listContainer.innerHTML);
+//   }
+//   function showTask(){
+//     listContainer.innerHTML = localStorage.getItem("data");
+//   }
+//   showTask();
+

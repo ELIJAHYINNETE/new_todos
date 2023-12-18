6015a4ec2dc1dcf425464e0d4e693d7cc9bb30e3
ELIJAHYINNETE <elijah.emdal@gmail.com>
2023-12-18 11:33:31 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -1,43 +1,57 @@
-
-function addTask() {
-  var taskInput = document.getElementById('input-box');
-  var todoList = document.getElementById('list-container');
-  //var addBtn = document.getElementById('btn');
-  if (taskInput.value.trim() !== ''){
-    var taskItem = document.createElement('li');
-    taskItem.className = 'todo-item';
-    var taskText=document.createElement('span');
-      taskText.textContent = taskInput.value;
-      taskItem.appendChild(taskText);
-
-      
-  
-      var deleteButton = document.createElement('button');
-       deleteButton.className = 'delete-button';
-      deleteButton.textContent = 'Delete';
-      deleteButton.onclick = function ()
-    {
-      todoList.removeChild(taskItem);
-    }
-    todoList.appendChild(taskItem);
-    taskInput.value='';
+let taskInput = document.getElementById("input-box");
+const enter= document.getElementById("btn")
+let todoList = document.getElementById('list-container');
+let ulEl=document.getElementById("list-container")
+let itemsel=document.getElementById("itemsEl")
+let typed=[]
+let theList=""
+let clearAll= document.getElementById("clearEl")
 
 
-  
-   }
-  
-  };
-
-let keypress = document.querySelector("#input-box");
-keypress.addEventListener('keydown',function (event){
-  if (event.keyCode === 13){
-    event.preventDefault();
-    addTask(event);
-
-
+enter.addEventListener("click",function(){
+  typed.push(taskInput.value)
+  let lastElment= typed[typed.length -1]
+  for (let i = 0; i<typed.length; i++){
+    itemsel.textContent= i+1 + " items"
+    
+   
+   
+   
   }
-}  
-);
+  theList +=` 
+  <li>
+  <input type="checkbox" id="chckBox" >${lastElment}
+  </li>`
+  
+
+
+  ulEl.innerHTML=theList
+  taskInput.value=""
+
+  
+
+
+})
+
+
+document.addEventListener('keypress', (event)=>{
+
+  let keyCode = event.keyCode ? event.keyCode : event.which;
+  if(keyCode === 13) {
+    btn.click();
+  } 
+});
+
+clearAll.addEventListener("click",function(){
+  typed=[]
+
+  ulEl.innerHTML=""
+  itemsel.textContent= "No item"
+})
+
+
+   
+
 
 
 
